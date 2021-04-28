2b06d295d7cfb82637c9321e79e7b6309b7c2e81
ben18785 <ben.c.lambert@gmail.com>
2021-04-28 16:09:25 +0100

    Added coverage checks

--- R/utils.R
+++ R/utils.R
@@ -3,7 +3,7 @@
 #' Returns a melatonin suppression response for a given level of log_lux
 #'
 #' @param log_lux a log lux value
-#' @param log_ed50 the log of the ed50 lux
+#' @param p1 the log of the ed50 lux
 #' @param p2 a positive shape parameter
 #'
 #' @return
@@ -12,6 +12,22 @@
 #' @examples
 #' # below returns 0.5 since log_lux=log_ed50
 #' logistic_2(10, 10, 5)
-logistic_2 <- function(log_lux, log_ed50, p2) {
-  return(1 - 1 / (1 + (log_lux / log_ed50)^p2))
+logistic_2 <- function(log_lux, p1, p2) {
+  return(1 - 1 / (1 + (log_lux / p1)^p2))
+}
+
+#' Calculates ed for a given quantile
+#'
+#' @param edx a quantile in [0, 1]
+#' @param p1 the log of the ed50 lux
+#' @param p2 a positive shape parameter
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' # at ed50 the returned value is 10^p1
+#' ed(0.5, 1.3, 4.4) # = 10^1.3
+ed <- function(edx, p1, p2) {
+  return(10^(p1 * (-1 + 1 / (1 - edx))^(1 / p2)))
 }
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -4,3 +4,8 @@ test_that("logistic function returns correct values", {
   p2 <- 3
   expect_equal(0.5, logistic_2(x, p1, p2))
 })
+
+test_that("ed function returns correct values",{
+  ## relies on ed50 = 10^p1
+  expect_equal(ed(0.5, 1.3, 4.2), 10^1.3)
+})
